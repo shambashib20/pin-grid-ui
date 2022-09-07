7347fbb71c0acdbddaf8cffb69ccbfef711a4a07
Tuhin Roy <tuhinroy881@gmail.com>
2022-09-07 15:18:59 +0530

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.css';
+import PinInputGrid from './PinInputGrid';
+import { validateInput } from './api';
+
+const PIN_LENGTH = 4;
 
 function App() {
+  const [pin, setPin] = useState<Array<number | undefined>>(
+    new Array(PIN_LENGTH)
+  );
+  const [validationResult, setValidationResult] = useState<boolean | undefined>(
+    undefined
+  );
+  const [validationMessage, setValidationMessage] = useState<
+    string | undefined
+  >(undefined);
+
+  const [isValidating, setIsValidating] = useState(false);
+
+  const onPinChanged = (pinEntry: number | undefined, index: number) => {
+    const newPin = [...pin];
+    newPin[index] = pinEntry;
+    setPin(newPin);
+  };
+
+  const validatePin = useCallback(async () => {
+    setIsValidating(true);
+    try {
+      const result = await validateInput(pin.join(''));
+      console.log(result);
+      setValidationResult(true);
+      setValidationMessage(result);
+    } catch (e) {
+      console.log(e);
+      setValidationMessage(e);
+      setValidationResult(false);
+      setPin(new Array(PIN_LENGTH));
+    } finally {
+      setIsValidating(false);
+    }
+  }, [pin]);
+
+  useEffect(() => {
+    const checkPin = async () => {
+      if (!pin.includes(undefined)) {
+        await validatePin();
+      }
+    };
+
+    checkPin();
+  }, [pin, validatePin]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PinInputGrid
+        isValidating={isValidating}
+        validationMessage={validationMessage}
+        validationResult={validationResult}
+        onPinChanged={onPinChanged}
+        pin={pin}
+        pinLength={PIN_LENGTH}
+      />
+
+      <button onClick={validatePin}>Validate</button>
     </div>
   );
 }
---
+++ src/PinInputGrid.tsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useRef } from 'react';
+import { StyledPinInput, ValidationResultParagraph } from './Pin.components';
+import { removeValuesFromArray } from './pin.utils';
+
+
+interface PinInputGridProps {
+    pin: Array<number | null>;
+    onPinChanged: (pinEntry: number | undefined, index: number) => void;
+    pinLength: number;
+    validationMessage: string | undefined;
+    validationResult: boolean | undefined;
+    isValidating: boolean;
+}
+
+const PIN_MIN_VALUE = 0;
+const PIN_MAX_VALUE = 9;
+const BACKSPACE_KEY = 'Backspace';
+
+
+const PinInputGrid: React.FC<PinInputGridProps> =  ({
+    pinLength, 
+    pin,
+    onPinChanged,
+    validationMessage,
+    validationResult,
+    isValidating,
+}) => {
+    const inputRefs = useRef<HTMLInputElement[]>([]);
+
+    const changePinFocus = (pinIndex: number) => {
+        const ref = inputRefs.current[pinIndex];
+        if (ref) {
+            ref.focus();
+        }
+    
+    };
+    
+    useEffect(() => {
+        changePinFocus(0);
+    }, [isValidating]);
+    
+    const onChange = (
+        event: React.ChangeEvent<HTMLInputElement>,
+        index: number
+    ) => {
+        const previousValue = event.target.defaultValue;
+        const valuesArray = event.target.value.split('');
+        removeValuesFromArray(valuesArray, previousValue);
+        const value = valuesArray.pop();
+        if (!value) {
+          return;
+        }
+        const pinNumber = Number(value.trim());
+        if (isNaN(pinNumber) || value.length === 0) {
+          return;
+        }
+
+        if (pinNumber >= PIN_MIN_VALUE && pinNumber <= PIN_MAX_VALUE) {
+          onPinChanged(pinNumber, index);
+          if (index < pinLength - 1) {
+            changePinFocus(index + 1);
+          }
+        }
+    };
+
+    const onKeyDown = (
+        event: React.KeyboardEvent<HTMLInputElement>,
+        index: number
+
+    ) => {
+        const keyboardKeyCode = event.nativeEvent.code;
+        if (keyboardKeyCode !== BACKSPACE_KEY) {
+            return;
+        }
+
+        if (pin[index] === undefined) {
+            changePinFocus(index - 1);
+        } else {
+            onPinChanged(undefined, index);
+        }
+    };
+
+
+    return (
+        <>
+      <div>
+        {Array.from({ length: pinLength }, (_, index) => (
+          <StyledPinInput
+            disabled={isValidating}
+            isCorrect={validationResult}
+            onKeyDown={(event) => onKeyDown(event, index)}
+            key={index}
+            ref={(el) => {
+              if (el) {
+                inputRefs.current[index] = el;
+              }
+            }}
+            onChange={(event) => onChange(event, index)}
+            value={pin[index] || ''}
+          />
+        ))}
+      </div>
+      <ValidationResultParagraph isCorrect={validationResult}>
+        {validationMessage}
+      </ValidationResultParagraph>
+    </>
+    );
+};
+
+export default PinInputGrid;
